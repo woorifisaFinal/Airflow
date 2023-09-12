218a37638f182c908149e940f2f8e59002101265
Joohyuk-Lee <54299329+Joohyuk-Lee@users.noreply.github.com>
2023-09-12 18:47:09 +0900

    add files

--- dags/stage1_infer_rds.py
+++ dags/stage1_infer_rds.py
@@ -22,17 +22,19 @@ def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
 
 default_args = {
     'owner': 'joohyuk',  # 이 작업의 오너입니다. 보통 작업을 담당하는 사람 이름을 넣습니다.
-    'schedule_interval': '@daily',
-    'start_date': datetime(2022, 1, 1),
+    # 'schedule_interval': '@daily',
+    'start_date': datetime(2023, 9, 12),
+    "schedule_interval":"*/2  * * * *", # 매일 06:00에 실행합니다.
     'tags': ['temp'],
 }
-#     start_date=days_ago(2), # DAG 정의 기준 2일 전부터 시작합니다.
-# 	  # start_date=datetime(2023, 8, 7, hour=12, minute=30), # DAG 정의 기준 시간부터 시작합니다
-#     # start_date=airflow.utils.dates.days_ago(14),  
+# start_date=datetime(2023, 8, 7, hour=12, minute=30),
+    # start_date=days_ago(2), # DAG 정의 기준 2일 전부터 시작합니다.
+	 # DAG 정의 기준 시간부터 시작합니다
+    # start_date=airflow.utils.dates.days_ago(14),  
 # 			# 스케쥴의 간격과 함께 DAG 첫 실행 시작 날짜를 지정해줍니다.
 #       # 주의: 1월 1일에 DAG를 작성하고 자정마다 실행하도록 schedule_interval을 지정한다면 태스크는 1월 2일 자정부터 수행됩니다	
 #     end_date=datetime(year=2023, month=8, day=19),
-# 		# schedule_interval="0 6 * * *", # 매일 06:00에 실행합니다.
+		
 # 		schedule_interval="@daily", # DAG 실행 간격 - 매일 자정에 실행
 
 # default_args = {
