64b4a8711659774076493d1cc71f47d3514aac76
Joohyuk-Lee <54299329+Joohyuk-Lee@users.noreply.github.com>
2023-09-18 10:03:01 +0900

    add filess

---
+++ model_backend/blackLitterman.py
@@ -0,0 +1,112 @@
+'''17~20년의 수익률 데이터를 기반으로 현재 시점에서 30일간 보유하고 있을 때 최적의 포트폴리오 전략을 도출하는 전통적 기법(블랙리터만)
+view = 전망에 들어가는 데이터는 현재 시점에서 30일 후 수익률을 예측한 데이터입니다. '''
+
+
+#Usual Suspects
+import pandas as pd
+import numpy as np
+
+
+# Use PyPortfolioOpt for Calculations
+from pypfopt import EfficientFrontier, objective_functions
+from pypfopt import black_litterman, risk_models
+from pypfopt import BlackLittermanModel, plotting
+from pypfopt import DiscreteAllocation
+
+from stage1 import getPrediction
+
+import json
+
+def runBlack():
+  asset_market = {
+      'us': 40719661.400,
+      'uk': 2949.56, 
+      'jp': 6017876.009,
+      'euro': 5558284.855,
+      'kor': 1993126.619,
+      'ind': 3728884.848,
+      'tw': 1712611.47,
+      'br': 959887.723,
+      'kor3y': 22.66,
+      'kor10y': 8.035,
+      'us3y': 26.03,
+      'us10y': 22.935,
+      'gold': 12996.0
+      } # 시가총액 (23년 7월 기준)
+
+  col_list = ['us', 'uk', 'jp', 'euro', 'kor', 'ind', 'tw', 'br', 'kor3y', 'kor10y', 'us3y', 'us10y', 'gold'] # list(asset_market.keys())
+
+  rename_dict = {
+      'kospi':"kor",
+      'nasdaq':"us",
+      'euro_stoxx':"euro",
+      'ftse':"uk",
+      'nikkei':"jp",
+      'korea_bond_03':"kor3y",
+      'korea_bond_10':"kor10y",
+      'america_bond_03':"us3y",
+      'america_bond_10':"us10y",
+      'gold':"gold",
+      'brazil':"br",
+      'taiwan':"tw",
+      'india':"ind"
+      }
+  
+  def get_portion(asset_market):
+    sum_value = sum(asset_market.values())
+    for k, v in asset_market.items():
+        asset_market[k] = v/sum_value
+        
+    return pd.Series(asset_market)
+
+
+  def get_excess_returns():
+      # 2022년 수익률
+      original_closes_data = pd.read_csv("/opt/airflow/data/close.csv", index_col=0)
+      excess_returns = original_closes_data.pct_change().dropna(axis=0) # 자산 수익률 - 무위험 수익률 => 초과 수익률, but 무위험 수익률을 꼭 연산하지 않아도 된다.
+      # 종가들을 opt/airflow에 저장할 때 이미 1년값 모두 가져오기 때문에 설정 필요 없음 
+      # condition = ("2021-11-30"<=excess_returns.index ) * (excess_returns.index <"2022-11-30")
+      # excess_returns = excess_returns[condition]
+      # column 명 제대로 되어 있는 RDS에서 가져오는 것이므로 rename 필요 없다.
+      # excess_returns.rename(columns=rename_dict, inplace=True)
+      excess_returns = excess_returns[col_list]
+      return excess_returns
+  
+  asset_market = get_portion(asset_market)
+  excess_returns = get_excess_returns()
+  median = excess_returns.median()
+
+  # Sigma = risk_models.CovarianceShrinkage(excess_returns).ledoit_wolf()
+  Sigma = risk_models.CovarianceShrinkage(excess_returns).shrunk_covariance()
+  lambd = black_litterman.market_implied_risk_aversion(asset_market)
+  market_prior = black_litterman.market_implied_prior_returns(asset_market, lambd, Sigma)
+
+  
+  viewdict = getPrediction().mean()
+
+
+  # bl = BlackLittermanModel(Sigma, pi=market_prior, absolute_views=viewdict)
+  bl = BlackLittermanModel(Sigma, pi="market", absolute_views=viewdict,market_caps=asset_market, risk_aversion=lambd)
+  ret_bl = bl.bl_returns()
+  # rets_df = pd.DataFrame([market_prior, ret_bl, pd.Series(median)],
+  #             index=["Prior", "Posterior", "Views"]).T
+  S_bl = bl.bl_cov()
+
+  ef = EfficientFrontier(ret_bl, S_bl)
+  ef.add_objective(objective_functions.L2_reg)
+  ef.max_sharpe()
+  weights = ef.clean_weights()
+#   weights = weights.astype('float')
+#   weights['type'] = 'B/공격형'
+#   weights['type'] = weights['type'].astype(str)
+
+  ef_ = EfficientFrontier(ret_bl, S_bl)
+  ef_.min_volatility()
+  weights_ = ef_.clean_weights()
+#   weights_ = weights.astype('float')
+#   weights_['type'] = 'B/안정형'
+#   weights_['type'] = weights_['type'].astype(str)
+
+  result1 = json.dumps(weights, ensure_ascii=False, sort_keys=False)
+  result2 = json.dumps(weights_, ensure_ascii=False, sort_keys=False)
+  return result1, result2
---
+++ model_backend/everyday.py
@@ -0,0 +1,112 @@
+import pandas as pd
+
+from CustomModel import custom_model
+from AlphaModel import alpha_model
+from blackLitterman import runBlack
+import datetime
+import sys
+import time
+import pymysql
+from sqlalchemy import create_engine
+import json
+import logging
+import yfinance as yf
+
+host = 'team5-db.cvqn3ewwzknb.ap-northeast-2.rds.amazonaws.com'
+username = 'admin'
+password = 'woorifisa'
+## test 하려고 test schema 만듬요
+database_name = 'woorifisa'
+port = '3306'
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+try:
+  db_url = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database_name}'
+  engine = create_engine(db_url)
+  # connection = pymysql.connect(host= host, user= username, passwd= password, db= database_name, connect_timeout=5)
+  connection = engine.connect()
+except pymysql.MySQLError as e:
+  logger.error('연결실패!')
+  logger.error(e)
+  sys.exit()
+
+logger.info('연결 성공')
+def saveTodayPortfolio():
+  ##FOR PROJECT
+  # now = datetime.datetime.now()
+  # today = now.strftime("%Y-%m-%d")
+  ##FOR LOCAL
+  today = '2022-08-01'
+
+
+  a1, a2 = alpha_model()
+
+
+  b1, b2 = runBlack()
+  b1_ = json.loads(b1)
+  b2_ = json.loads(b2)
+
+  c1, c2 = custom_model(today)
+
+
+  a1 = pd.DataFrame(a1).T
+  a1 = a1.astype('float')
+  a1['type'] = "A/안정형"
+  a1['type'] = a1['type'].astype(str)
+    
+  a2 = pd.DataFrame(a2).T
+  a2 = a2.astype('float')
+  a2['type'] = 'A/공격형'
+  a2['type'] = a2['type'].astype(str)
+
+  b1 = pd.json_normalize(b1_)
+  b1 = b1.astype('float')
+  b1['type'] = 'B/공격형'
+  b1['type'] = b1['type'].astype(str)
+
+  b2 = pd.json_normalize(b2_)
+  b2 = b2.astype('float')
+  b2['type'] = 'B/안정형'
+  b2['type'] = b2['type'].astype(str)
+
+  c1 = c1.astype('float')
+  c1['type'] = "C/안정형"
+  c1['type'] = c1['type'].astype(str)
+
+  c2 = c2.astype('float')
+  c2['type'] = 'C/공격형'
+  c2['type'] = c2['type'].astype(str)
+
+
+  df = pd.concat([b1, b2, a1, a2, c1, c2])
+  df['date'] = today
+  df.to_sql(name='portfolio', con=connection, index=False, if_exists='append',  schema='woorifisa')
+  return 0
+
+saveTodayPortfolio()
+
+
+
+def getClose():
+  ##FOR PROJECT
+  # now = datetime.datetime.now()
+  # today = now.strftime("%Y-%m-%d")
+  # tomorrow = now + datetime.timedelta(days=1)
+  # tomorrow = tomorrow.strftime("%Y-%m-%d")
+  ##FOR LOCAL
+  start='2017-01-01'
+  end='2023-08-31' 
+  symbol = ['^IXIC','^FTSE','^N225','^STOXX50E','^KS11','^BVSP','^TWII','^BSESN', 'GC=F']
+  col_list = ['us', 'uk', 'jp', 'euro', 'kor', 'ind', 'tw', 'br', 'gold'] 
+  df = yf.download(symbol, start, end)
+  dfs = df['Close']
+  rename_dict = {old_col: new_col for old_col, new_col in zip(dfs.columns, col_list)}
+  dfs = dfs.rename(columns=rename_dict)
+
+  dfs.to_sql(name='symboltest', con=connection, index=True, if_exists='append',  schema='woorifisa')
+  connection.commit()
+  return 0
+
+# getClose()
